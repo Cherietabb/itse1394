593cf898fb23d186ee56cbb05815da600e78442a
cherietabb <cherietabb@gmail.com>
2015-09-15 21:41:26 -0500

    moved on with requirement 2

--- README.md
+++ README.md
@@ -10,8 +10,14 @@
 
 ### Requirement 1 ###
 
-1. User can enter a Fahrenheit temperature and the page displays a message
-based on the value entered. Use if-else-if statements. Use reasonable
-judgment to determine if the appropriate message should be cold, chilly,
-freezing, warm, hot, etc. Produce an error message if the value entered in
-less than -50 or greater than 140. Use try/catch to handle the error.
+1.  User can enter a Fahrenheit temperature and the page displays a message
+    based on the value entered. Use if-else-if statements. Use reasonable
+    judgment to determine if the appropriate message should be cold, chilly,
+    freezing, warm, hot, etc. Produce an error message if the value entered in
+    less than -50 or greater than 140. Use try/catch to handle the error.
+
+### Requirement 2 ###
+
+2.  User can enter a number between 1-12 (error message otherwise – use
+    try/catch to handle the error). Use a switch statement to output the month
+    name based on the number entered.
--- program-03/program03.js
+++ program-03/program03.js
@@ -3,15 +3,15 @@
  */
 function myTemp() {
     var x = document.getElementById('temp');
-    var err = 'invalid';
-
-    try {
-        if (x > 140);
-        if (x < -50);
-    }
-    catch(err) {
-        console.log("Input is " + err);
-    }
+    //var err = 'invalid';
+    //
+    //try {
+    //    if (x > 140);
+    //    if (x < -50);
+    //}
+    //catch(err) {
+    //    console.log("Input is " + err);
+    //}
 
     if (x > 90) {
         console.log('foo');
@@ -22,7 +22,64 @@ function myTemp() {
         console.log('Pleasant');
     } else if ((x <= 60) && (x >= 40)) {
         console.log('Chilly');
-    } else {
-        console.log('Burr!');
-    }
+    } 
 }
+// Requirement 2
+
+
+
+function getMonth() {
+    var x = document.getElementById('month');
+    var month = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    var b = month[x];
+    var y = "";
+    try {
+        x < 1;
+        x > 12;
+    }
+    catch (e) {
+        alert('Number must be 1 - 12!');
+    }
+    switch (x) {
+        case 1:
+            b = 'January';
+            break;
+        case 2:
+            b = 'Februrary';
+            break;
+        case 3:
+            b = 'March';
+            break;
+        case 4:
+            b = 'April';
+            break;
+        case 5:
+            b = 'May';
+            break;
+        case 6:
+            b = 'June';
+            break;
+        case 7:
+            b = 'July';
+            break;
+        case 8:
+            b = 'August';
+            break;
+        case 9:
+            b = 'September';
+            break;
+        case 10:
+            b = 'October';
+            break;
+        case 11:
+            b = 'November';
+            break;
+        case 12:
+            b = 'December';
+            break;
+    }
+    document.write('You picked ' + b);
+
+};
