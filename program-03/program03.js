1c609e470fc556d2ce13a6b553ffc8506ea2adbe
cherietabb <cherietabb@gmail.com>
2015-09-16 22:33:40 -0500

    programs don't run correctly

--- program-03/program03.js
+++ program-03/program03.js
@@ -2,84 +2,85 @@
  * Created by Cherie Tabb on 9/14/2015.
  */
 function myTemp() {
-    var x,temp, message;
+    var x,temp, message, e;
     message = document.getElementById('temp');
     message.innerHTML = "";
     x = document.getElementById('temp').value;
+    e = "value outside accepted parameters";
     try {
-        if (x >= 90) {
+        if (x > 140) throw "invalid";
+        if ((x > 90) && (x <= 140)) {
             console.log('foo');
             alert('Hot!');
-        } else if ((x >= 80) && (x < 90)) {
-            alert('Mighty Warm');
+        } else if ((x >= 80) && (x <= 90)) {
+            alert('Mighty Warm!');
         } else if ((x > 60) && (x <= 70)) {
-            alert('Pleasant');
+            alert('Pleasant!');
         } else if ((x >= 40) && (x <= 60)) {
-            alert('Chilly');
-        } else if (x <= 39) {
-            alert('Burr!');
+            alert('Chilly!');
+        } else if ((x <= 39) && (x > -50)) {
+            alert('freezing!');
         } else {
             alert(x + ' degrees');
         }
-        if (x < -50 || x > 140) throw "value outside accepted parameters";
-        return false;
+        if (x < -50) throw "invalid";
     }
     catch(e) {
         message.innerHTML = e.message;
     }
-    return true
-
 }
 
 // Requirement 2
 
 function getMonth() {
-    var x, month, message;
-    message = document.getElementById('month');
-    message.innerHTML = "";
+    var message, x, text;
+    message = document.getElementById('message');
+
+    x = document.getElementById('month').value;
     month = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
-    x = document.getElementById('month').value;
+
     try {
         switch (x) {
             case 1:
-                month = 'January';
+                text = 'You picked ' + month;
                 break;
             case 2:
-                month = 'Februrary';
+                text = 'You picked ' + month;
                 break;
             case 3:
-                month = 'March';
+                text = 'You picked ' + month;
                 break;
             case 4:
-                month = 'April';
+                text = 'You picked ' + month;
                 break;
             case 5:
-                month = 'May';
+                text = 'You picked ' + month;
                 break;
             case 6:
-                month = 'June';
+                text = 'You picked ' + month;
                 break;
             case 7:
-                month = 'July';
+                text = 'You picked ' + month;
                 break;
             case 8:
-                month = 'August';
+                text = 'You picked ' + month;
                 break;
             case 9:
-                month = 'September';
+                text = 'You picked ' + month;
                 break;
             case 10:
-                month = 'October';
+                text = 'You picked ' + month;
                 break;
             case 11:
-                month = 'November';
+                text = 'You picked ' + month;
                 break;
             case 12:
-                month = 'December';
+                text = 'You picked ' + month;
                 break;
-
+            default:
+                break;
         }
         if (x < 1 || x > 12)
             alert('Number must be 1 - 12!');
@@ -88,6 +89,14 @@ function getMonth() {
     catch (e) {
         message.innerHTML = e.message;
     }
-    alert('You picked ' + month[x - 1]);
-    return true;
+    document.getElementById("demo").innerHTML = text;
 }
+
+// Requirement 3
+function getNum() {
+    var x, num, message;
+    message = document.getElementById('num');
+    message.innerHTML = "";
+
+}
+
