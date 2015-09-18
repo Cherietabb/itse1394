b955599f983da60f8e2c52d21d4f7449fedaeff3
cherietabb <cherietabb@gmail.com>
2015-09-18 00:22:45 -0500

    added requirement 4

--- program-03/program03.js
+++ program-03/program03.js
@@ -1,8 +1,10 @@
 /**
  * Created by Cherie Tabb on 9/14/2015.
  */
+
+// Requirement 1
 function myTemp() {
-    var x,temp;
+    var x, temp;
     x = document.getElementById('temp').value;
     try {
         if ((x > 90) && (x <= 140)) {
@@ -17,11 +19,12 @@ function myTemp() {
         } else if ((x > -50) && (x <= 39)) {
             alert('freezing!');
         }
-        if (x < -50) throw {message: "invalid input"}
-        if (x > 140) throw {message: "invalid input"}
+        if (x < -50) throw {message: "invalid input"};
+        if (x > 140) throw {message: "invalid input"};
+
     }
-    catch(e) {
-        alert("Error: "+e.message)
+    catch (e) {
+        alert("Error: " + e.message)
     }
 }
 
@@ -33,69 +36,105 @@ function getMonth() {
     month = [
         'January', 'February', 'March', 'April', 'May', 'June',
         'July', "August", 'September', 'October', 'November', 'December'];
+    //alert('Strawberries');
+
     try {
-        switch (x) {
-            case 1:
-                console.log('tree');
-                alert('January');
-                break;
-            case 2:
-                alert('February');
-                break;
-            case 3:
-                alert('March');
-                break;
-            case 4:
-                alert('April');
-                break;
-            case 5:
-                alert('May');
-                break;
-            case 6:
-                alert('June');
-                break;
-            case 7:
-                alert('July');
-                break;
-            case 8:
-                alert('August');
-                break;
-            case 9:
-                alert('September');
-                break;
-            case 10:
-                alert('October');
-                break;
-            case 11:
-                alert('November');
-                break;
-            case 12:
-                alert('December');
-                break;
-            default:
-                alert('Strawberries')
-                break;
+        if (x === 1) {
+            alert('January');
+        } else if (x === 2) {
+            alert('February');
+        } else if (x === 3) {
+            alert('March');
+        } else if (x === 4) {
+            alert('April');
+        } else if (x === 5) {
+            alert('May');
+        } else if (x === 6) {
+            alert('June');
+        } else if (x === 7) {
+            alert('July');
+        } else if (x === 8) {
+            alert('August');
+        } else if (x === 9) {
+            alert('September');
+        } else if (x === 10) {
+            alert('October');
+        } else if (x === 11) {
+            alert('November');
+        } else if (x === 12) {
+            alert('December');
         }
-        if (x < 1 || x > 12)
-            throw {message: 'Number must be 1 - 12!'}
-        if (isNaN(x))
-            throw {message: 'Must enter a number!'}
-        if (x = "")
-            throw {message: 'input is empty!'}
-        }
+        if (x < 1) throw {message: 'Too low!'};
+        if (x > 12) throw {message: 'Number must be 1 - 12!'};
+        if (isNaN(x)) throw {message: 'Must enter a number!'};
+        if (x === "") throw {message: 'input is empty!'}
+    }
     catch (e) {
-        alert("Error: "+e.message)
+        alert("Error: " + e.message);
     }
 }
 
 // Requirement 3
 function getNum() {
-    var x, num, message;
-    message = document.getElementById('message');
-    message.innerHTML = "";
-    x = document.getElementById('num');
+    var x, num;
+    x = document.getElementById('num').value;
+    num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    try {
+        switch (x) {
+            case 12:
+            case 1:
+            case 2:
+                alert('Winter');
+                break;
+            case 3:
+            case 4:
+            case 5:
+                alert('Spring');
+                break;
+            case 6:
+            case 7:
+            case 8:
+                alert('Summer');
+                break;
+            case 9:
+            case 10:
+            case 11:
+                alert('Fall');
+                break;
+            default:
 
-
-
+        }
+        if (x < 1 || x > 12)
+            throw {message: 'Number must be 1 - 12!'};
+        if (isNaN(x))
+            throw {message: 'Must enter a number!'};
+        if (x === "")
+            throw {message: 'value is empty!'}
+    }
+    catch (e) {
+        alert('Error: ' + e.message)
+    }
 }
 
+function displayBirthMonth(month) {
+     month = {
+         'January': 'Zion',
+         'February': 'Clarice',
+         'March': 'Mark',
+         'April': 'Glenda',
+         'May': 'Mary',
+         'June': 'Nicholas',
+         'July': 'Pat',
+         'August': 'Mary',
+         'September': 'Theresa',
+         'October': 'Heather',
+         'November': 'Cherie',
+         'December': 'Alicia'
+     };
+    var text = "";
+    for (var key in month) {
+        text += month[key] + '<br>'
+
+    }
+    document.getElementById('birth').innerHTML = text;
+};
