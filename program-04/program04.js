c458a53440e17db7c6e6d1201c3c5d4e8a718ede
cherietabb <cherietabb@gmail.com>
2015-09-21 21:57:11 -0500

    added content to req 4

--- program-04/program04.js
+++ program-04/program04.js
@@ -1,10 +1,21 @@
 /**
  * Created by Cherie on 9/15/2015.
  */
-//Requirement 1
 
+//Requirement 1
+var findAverage = function(a, b, c ,d) {
+    for (var i=0, total = 0, len=arguments.length; i<len; i++) {
+        total += arguments[i];
+    }
+    return total / arguments.length;
+};
+document.getElementById('num').innerHTML = findAverage(50, 6, 5, -50);
 
 //Requirement 2
+var selfInvoke = function () {
+    return "#2 Hello from SelfInvoke";
+};
+document.getElementById('greeting').innerHTML = selfInvoke();
 
 
 // Requirement 3
