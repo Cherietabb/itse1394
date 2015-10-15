08f27fe6c3b00ef938c2d5d239517b581200ca1c
Cherie Tabb <Cherietabb@gmail.com>
2015-10-15 14:10:10 -0500

    started program 8

--- program-08/program-08.js
+++ program-08/program-08.js
@@ -1,3 +1,12 @@
 /**
  * Created by Cherie on 10/4/2015.
  */
+
+// Requirement 1
+$(document).ready(function() {
+    $('#showText').click(function() {
+        var txt = '';
+        txt = 'Show some txt';
+        $('p').html(txt);
+    })
+});
