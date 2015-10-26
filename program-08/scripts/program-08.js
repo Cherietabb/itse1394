cbcb76b87d1a51ce91cd9059dfee679e5296aa5b
cherietabb <cherietabb@gmail.com>
2015-10-25 20:49:22 -0500

    add pics and update prog 8

--- program-08/scripts/program-08.js
+++ program-08/scripts/program-08.js
@@ -29,26 +29,49 @@ $('#setVal').click(function () {
 });
 
 // Requirement 4
-$(document).ready(function() {
-    $('#weekend').click(function() {
-        alert('Color Change!');
+$(document).ready(function () {
+    $('#weekend').click(function () {
         $('#warrior').css('background-color', '#ff66cc');
+        alert('Color Change!');
+        $('#warrior').css('background-color', '');
     });
 });
 
 // Requirement 5
-$('#rockin').on('focus', function() {
-    var text1 = document.createElement('li');
-    var text2 = $('<li></li>').text(text3);
-    var text3 = $('#rockin').val();
-    $('body').append(text1,text2,text3);
+$('#rockin').change(function () {
+    $(this).each(function () {
+        var newList = $(this).val();
+        $('ol').append('<li>' + newList);
+    });
 });
 
-
 // Requirement 6
-
+$(document).ready(function () {
+    $('#forever').click(function () {
+        $('img').before('<img src="assets/buckingham_fountain.jpg"/>' + '<br>' +
+            '<img src="assets/tribune_bldg.jpg"/>' + '<br>');
+    });
+    $('#home').click(function () {
+        $('#pics').after('<img src="assets/black_and_brown.jpg"/>' + '<br>' + '<img src="assets/corgi_puppy.jpg"/>' +
+            '<br>' + '<img src="assets/siberian_husky_puppy.jpg"/>' + '<br>')
+    });
+    /*
+     var images = {'image1': 'assets/chicago_at_night.jpg', 'image2': 'assets/buckingham_fountain.jpg'};
+     $('img').before('images[0]' + '<br>' + 'images[1]' + '<br>');
+     */
+});
 
 // Requirement 7
 
 
 // Requirement 8
+$(document).ready(function () {
+    $('#activateToggle').on({
+        mouseover: function () {
+            $('.toggleClass').css({'background-color': 'red', 'height': '50px', 'width': '50px'});
+        },
+        mouseout: function () {
+            $('.toggleClass').css({'background-color': '', 'height': '', 'width': ''});
+        }
+    });
+});
