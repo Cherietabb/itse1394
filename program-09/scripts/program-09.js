c12289e3ad08b868a262906560e792e7049c240e
Cherie Tabb <Cherietabb@gmail.com>
2015-10-26 14:34:24 -0500

    requirement 1 complete

--- program-09/scripts/program-09.js
+++ program-09/scripts/program-09.js
@@ -1,3 +1,10 @@
 /**
  * Created by Cherie on 10/4/2015.
  */
+
+// Requirement 2
+$(document).ready(function () {
+    $('#places').click(function() {
+        $('p').parent('header footer').css('border', 'blue');
+    });
+});
