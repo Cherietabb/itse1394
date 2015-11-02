fa3ac6dfc464b6d72494c451b41f9ad2898ab212
cherietabb <cherietabb@gmail.com>
2015-11-02 00:22:07 -0600

    program 9 complete

--- program-09/scripts/program-09.js
+++ program-09/scripts/program-09.js
@@ -4,14 +4,35 @@
 
 // Requirement 2
 $(document).ready(function () {
-    $('#places').click(function() {
-        $('p, div').parent().addClass('blue');
+    $('#places').click(function () {
+        $('p, div').parent().toggleClass('blue');
     });
-    $('#places').click(function() {
-        $('p, div').parent().removeClass('blue');
-    })
-
 });
 
-
 // Requirement 3
+$(document).ready(function () {
+    $('#happy').click(function () {
+        $('#day').parents().toggleClass('red');
+    })
+});
+
+// Requirement 4
+$(document).ready(function () {
+    $('#btnClick').click(function () {
+        $('nav, article').children().toggleClass('black');
+    })
+});
+
+// Requirement 5
+$(document).ready(function () {
+    $('#descend').click(function() {
+        $('section').find('div').toggleClass('color');
+    })
+});
+
+// Requirement 6
+$(document).ready(function () {
+    $('#para').click(function() {
+        $('header').children().siblings('p').toggleClass('colors');
+    })
+});
