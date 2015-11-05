4c243dea266be053b56e1dc93777a45ea0ac8dbf
Cherie Tabb <Cherietabb@gmail.com>
2015-11-04 21:13:49 -0600

    working on req # 2

--- program-10/js/program-10.js
+++ program-10/js/program-10.js
@@ -3,6 +3,22 @@
  */
 
 // Requirement 2
+/*
 $(document).ready(function() {
+    $('#bibbity').click(function() {
+        $.get('ajax/program-10.txt', function(data) {
+            $('p').html(data);
+            alert('load is success');
+        })
+    })
+});
+*/
 
+$(document).ready(function () {
+    $('#bibbity').click(function () {
+        $.get('program-10.txt', function (data) {
+            alert(data);
+            $('#p2').html(data);
+        })
+    })
 });
