e88c4d196fabbe7cf2e95c9ec6d082db12e5998b
cherietabb <cherietabb@gmail.com>
2015-11-17 01:19:30 -0600

    all but one complete

--- program-12/scripts/program-12.js
+++ program-12/scripts/program-12.js
@@ -15,15 +15,50 @@ $(document).ready(function () {
     $('#to').autocomplete({
         source: daysOfWeek
     })
+
 });
 
 // Requirement 3
-
+$(document).ready(function () {
+    $('input[type=submit], a, button').button();
+    $('checkbox').buttonset();
+});
 
 // Requirement 4
+$(document).ready(function () {
+    $('#picker').datepicker({
+        beforeShowDay: function (date) {
+            var dayOfWeek = date.getDay();
+            if (dayOfWeek == 0 || dayOfWeek == 6) {
+                return [false];
+            } else {
+                return [true];
+            }
 
+        }
+    });
+});
 
 // Requirement 5
-
+$(function(){
+    $( "#log" ).dialog({
+        autoOpen: false,
+        buttons: {
+            OK: function() {
+                $( this ).dialog( "close" );
+            }
+        },
+        beforeClose: function( event, ui ) {
+            if ( !$( "#terms" ).prop( "checked" ) ) {
+                event.preventDefault();
+                $( "[for=terms]" ).addClass( "invalid" );
+            }
+        },
+        width: 400
+    });
+    $( "#open" ).click(function() {
+        $( "#log" ).dialog( "open" );
+    });
+});
 
 // Requirement 6
