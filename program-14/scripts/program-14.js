127f763433e19bc5d5aec775ff920a138dde50f9
cherietabb <cherietabb@gmail.com>
2015-12-02 22:37:43 -0600

    working on req 3

--- program-14/scripts/program-14.js
+++ program-14/scripts/program-14.js
@@ -11,12 +11,50 @@ $(document).ready(function () {
 
 // Requirement 2
 $(document).ready(function () {
-    var change = $('#div-animate');
-    change.click().animateClass('class4', 1000);
+    $('#div-animate').click(function () {
+        $(this).animate(
+            {
+                backgroundColor: '#0FF'
+            }
+        )
+    })
 });
 
 // Requirement 3
+$(document).ready(function () {
+    function runEffect() {
+        $('#blindDiv').hide('blind', {direction: 'horizontal'}, 1000, callback);
+    }
+    function callback() {
+        setTimeout(function() {
+            $('#blindDiv').removeAttr('style').hide().fadeIn();
+        }, 1000);
+    }
+    $('#blindDiv').click(function () {
+        runEffect();
+        return false;
+    });
+    function runClip() {
+        $('#clipDiv').hide('clip', {direction: 'horizontal'}, 1000);
+    }
+    $('#clipDiv').click(function() {
+        runClip();
+        return false;
+    });
+    function runDrop() {
+        $('#dropDiv').hide('drop', {direction: 'horizontal'}, 1000, callback);
+    }
+    function callback() {
+        setTimeout(function() {
+            $('#dropDiv').removeAttr('style').show().fadeIn();
 
+        }, 1000);
+    }
+    $('#dropDiv').click(function () {
+        runDrop();
+        return false;
+    })
+});
 
 // Requirement 4
 
