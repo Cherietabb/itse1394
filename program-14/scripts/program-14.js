27e28be1e239201a17e0eed4cdc79603db4cc7e5
cherietabb <cherietabb@gmail.com>
2015-12-06 13:50:50 -0600

    problems with switchClass

--- program-14/scripts/program-14.js
+++ program-14/scripts/program-14.js
@@ -23,13 +23,14 @@ $(document).ready(function () {
 // Requirement 3
 $(document).ready(function () {
     function runEffect() {
-        $('#blindDiv').hide('blind', {direction: 'horizontal', distance: 100}, 1000, callback1);
+        $('#blindDiv').hide('blind', {direction: 'horizontal', distance: 100}, 2000, callback1);
     }
 
     function callback1() {
         setTimeout(function () {
-            $('#blindDiv').removeAttr('style').hide().fadeIn();
+            $('#blindDiv:visible').removeAttr('style').fadeIn();
         }, 1000);
+        $('#blindDiv').hide();
     }
 
     $('#blindDiv').click(function () {
@@ -37,77 +38,114 @@ $(document).ready(function () {
         return false;
     });
     $('#clipDiv').click(function () {
-        $(this).hide('clip', {direction: 'horizontal'}, 1000)
+        $(this).effect({
+            effect: 'clip',
+            duration: 2000,
+            easing: 'swing'
+        })
     });
+    // Drop Effect
     function runDrop() {
-        $('#dropDiv').show('drop', {direction: 'horizontal'}, 1000, callback2);
+        $('#dropDiv').show('drop', {times: 10, distance: 100}, 1000, callback2);
     }
 
     function callback2() {
         setTimeout(function () {
-            $('#dropDiv').removeAttr('style').show().fadeOut();
+            $('#dropDiv:visible').removeAttr('style').fadeOut();
 
         }, 1000);
     }
-
+    // Call Function
     $('#dropBtn').click(function () {
         runDrop();
         return false;
     });
+    // Hide the div
+    $('#dropDiv').hide();
+
+    // Explode Effect
     $('#explodeDiv').click(function () {
-        $(this).hide('explode', 1000)
+        $(this).effect({
+            effect: 'explode',
+            duration: 2000,
+            easing: 'easeOutQuad'
+        })
     });
+
+    // Fold Effect
     $('#foldDiv').click(function () {
-        $(this).toggle('fold', 1000);
+        $(this).effect({
+            effect: 'fold',
+            duration: 3000
+        })
     });
+
+    // Puff Effect
     function runPuff() {
-        $('#puffDiv').show('puff', {times: 10}, 1000, callback3);
+        $('#puffDiv').show('puff', {times: 10, distance: 100}, 1000, callback3);
     }
+
     function callback3() {
         setTimeout(function () {
-            $('#puffDiv').removeAttr('style').show().fadeOut();
+            $('#puffDiv:visible').removeAttr('style').fadeOut();
         },1000);
     }
+    // Call Function
     $('#puffBtn').click(function () {
         runPuff();
         return false;
     });
-    function runShake() {
-        $('#shakeDiv').hide('shake', {times: 5, distance: 100}, 1000, callback4);
-    }
-    function callback4() {
-        setTimeout(function() {
-            $('#shakeDiv').removeAttr('style').hide().fadeIn();
-        });
-        $('#shakeDiv').click(function () {
-            runShake();
-            return false;
-        })
-    }
+    // Hide Div
+    $( "#puffDiv" ).hide();
 
+    // Shake Effect
+    $('#shakeDiv').click(function () {
+        $(this).effect({
+            effect: 'shake',
+            times: 10,
+            distance: 50
+        }, 3000, function () {
+            $(this).hide();
+        })
+    });
+
+    // Slide Effect
+    $('#slideDiv').click(function () {
+        $(this).effect({
+            effect: 'slide',
+            duration: 1500
+        })
+    })
 });
 
 // Requirement 4
 $(document).ready(function () {
     $('#addPic').click(function () {
-       $('body').addClass('wallPaper');
+       $('body').addClass('wallPaper', 1000);
     });
     $('#rmPic').click(function () {
         $('body').removeClass('wallPaper')
     })
 });
 
-
 // Requirement 5
 $(document).ready(function () {
-    $('#bkgdChg').on('click',function () {
-        $('body').switchClass('no-wallpaper', 'new-wallpaper', 2000, 'easeInQuad');
-        return false;
+    $('#btnSwitch').click(function () {
+        $('.noWallpaper').switchClass('noWallpaper', 'newWallpaper', 2000, 'easeOutQuad');
+        $('.newWallpaper').switchClass('newWallpaper', 'noWallpaper', 2000, 'easeOutQuad');
     })
 });
 
-
 // Requirement 6
+$(document).ready(function () {
+    $('#switch').click(function () {
 
+    })
+});
 
 // Requirement 7
+$(document).ready(function () {
+    $('#button').click(function() {
+        $('#effect').toggleClass('newClass', 2000)
+    });
+});
